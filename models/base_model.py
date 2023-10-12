400a66a62c3f4e667cd2f59a281aa1bfc4b3a3a2
Your Name <you@example.com>
2023-10-13 00:57:26 +0100

    Task 4 added

--- models/base_model.py
+++ models/base_model.py
@@ -9,36 +9,35 @@ from datetime import datetime
 
 
 class BaseModel:
-    """
-    BaseModel defines all common
-    attributes/methods for other classes
-    """
-
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            # If kwargs is not empty, populate instance attributes from the dictionary
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            else:
+                self.id = str(uuid.uuid4())
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """this function returns the string representation
-            of an instance
-        """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dir__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """ updates the public instance attribute updated_at
-            with the current datetime
-        """
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.update_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
         return data
