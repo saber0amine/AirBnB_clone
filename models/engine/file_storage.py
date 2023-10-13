dc003c3a226163c7e28a8db1dce19d0d1fed745e
Your Name <you@example.com>
2023-10-13 18:17:27 +0100

    v1 storage file added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,13 +3,7 @@
 Serializes instances to a JSON file and deserializes JSON file to instances
 """
 import json
-from datetime import datetime
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+
 
 
 class FileStorage:
@@ -44,22 +38,9 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 dictofobjs = json.loads(f.read())
                 from models.base_model import BaseModel
-                from models.user import User
                 for key, value in dictofobjs.items():
                     if value['__class__'] == 'BaseModel':
                         FileStorage.__objects[key] = BaseModel(**value)
-                    elif value['__class__'] == 'User':
-                        FileStorage.__objects[key] = User(**value)
-                    elif value['__class__'] == 'Place':
-                        FileStorage.__objects[key] = Place(**value)
-                    elif value['__class__'] == 'State':
-                        FileStorage.__objects[key] = State(**value)
-                    elif value['__class__'] == 'City':
-                        FileStorage.__objects[key] = City(**value)
-                    elif value['__class__'] == 'Amenity':
-                        FileStorage.__objects[key] = Amenity(**value)
-                    elif value['__class__'] == 'Review':
-                        FileStorage.__objects[key] = Review(**value)
 
         except FileNotFoundError:
             pass
