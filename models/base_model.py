c23aa4947daede62a9cfcd189242bc7dddd32dd5
Your Name <you@example.com>
2023-10-13 01:23:49 +0100

    add test for the task 4

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
             if 'id' in kwargs:
                 self.id = kwargs['id']
             else:
-                self.id = str(uuid.uuid4())
+                self.id = str(uuid4())
             if 'created_at' in kwargs:
                 self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
             else:
@@ -25,7 +25,7 @@ class BaseModel:
             else:
                 self.updated_at = datetime.now()
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
